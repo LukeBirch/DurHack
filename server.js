7cbc66d92bf982f2256f995e87d461d5ce90a3e0
0genblik <92367373+0genblik@users.noreply.github.com>
2022-02-19 22:37:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const router = express.Router();
 const fs = require('fs');
 
-const app = express()
-const port = 3000
-
+//express app
+const app = express();
 app.use(express.static('client'));
-app.use(express.json());
+app.use(express.json())
 
-// require all necessary json files
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//json file
 const databaseData = require('./database.json');
 
 
-app.get('/one', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/timeinput', function (req, resp) {
-  
-  databaseData[req.body.person]["tod"] = req.body.time;
-  console.log(databaseData[req.body.person]["tod"])
-  fs.writeFileSync('./database.json', JSON.stringify(databaseData));
-  resp.send('Successfully added data');
-});
+app.post(`/frequency`,(req, res) => {
+    databaseData[req.body.person]["frequency"] = req.body.frequency;
+     fs.writeFileSync('./database.json', JSON.stringify(databaseData));
+    console.log(req.body.frequency)
+    res.send('yes')
+    });
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(8000);   
